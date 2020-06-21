e07dd8237f98985e30aae7da88845065d69f1c45
ahmedraza55592 <ahmedraza55592@users.noreply.github.com>
2020-06-21 19:19:22 +0500

    write some description

--- src/Child.js
+++ src/Child.js
@@ -6,7 +6,7 @@ const Child = () => {
     let counterValue = useContext(counterContext);
     return (
         <div>
-            <h3>This is first child Class</h3>
+            <h3>This is first child Class using context API</h3>
             <h3>Counter value is: {counterValue[0]} </h3>
             <button onClick={()=> {counterValue[1](++counterValue[0])}}>Increment Context</button>
         </div>
--- src/ReducerChild.js
+++ src/ReducerChild.js
@@ -6,7 +6,7 @@ const ReducerChild = () => {
     let [state, dispatch] = useReducer(counterReducer, 0);
     return (
         <div>
-            <h3>This is the second child </h3>
+            <h3>This is the second child using reducer</h3>
             <h3>Value of Reducer is {state}</h3>
             <button onClick={()=> {dispatch('INCREMENT')}}>Increment Reducer</button>
         </div>
